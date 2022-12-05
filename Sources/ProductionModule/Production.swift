2cfe72ae0e8c2816683e091f369cb792186a6f59
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-12-05 14:54:20 +0100

    Added `Production`.

---
+++ Sources/ProductionModule/Production.swift
@@ -0,0 +1,9 @@
+//
+//  Production.swift
+//  
+//
+//  Created by Jeremy Bannister on 12/5/22.
+//
+
+///
+public enum Production { }
---
+++ Tests/ProductionModule-tests/_exported_imports.swift
@@ -0,0 +1,9 @@
+//
+//  _exported_imports.swift
+//  
+//
+//  Created by Jeremy Bannister on 12/5/22.
+//
+
+///
+@_exported import ProductionModule
